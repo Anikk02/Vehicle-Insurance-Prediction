a48e4e6426d86fe69605dd8bc99a1907fa0afac6
Anikk02 <kiddoanyy0208@gmail.com>
2026-02-08 17:28:23 +0530

    Prepared project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    author = 'Aniket Paswan',
+    author_email = 'anikk0208@gmail.com',
+    packages = find_packages()
+)
